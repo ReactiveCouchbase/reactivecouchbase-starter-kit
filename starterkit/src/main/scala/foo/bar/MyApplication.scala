e6767b8fb72be4781050b7b0d187c36b336d049f
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-01-23 15:10:06 +0100

    Update starter kit

--- starterkit/src/main/scala/foo/bar/MyApplication.scala
+++ starterkit/src/main/scala/foo/bar/MyApplication.scala
@@ -1,7 +1,7 @@
 package foo.bar
 
 import play.api.libs.json._
-import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.{Future, Await, ExecutionContext}
 import org.reactivecouchbase.ReactiveCouchbaseDriver
 import scala.concurrent.duration.Duration
 import java.util.concurrent.TimeUnit
@@ -17,6 +17,7 @@ object Utils {
 object MyApplication extends App {
 
   import Utils._
+  import org.reactivecouchbase.CouchbaseRWImplicits.jsObjectToDocumentWriter
 
   val driver = ReactiveCouchbaseDriver()
   val bucket = driver.bucket("default")
@@ -28,14 +29,39 @@ object MyApplication extends App {
     "age" -> 42
   )
 
-  for (i <- 17 to 100) {
-    bucket.set(s"person-$i", Person("Jane", "Doe", i))
+  // persist the JSON doc with the key 'john-doe', using implicit 'jsObjectToDocumentWriter' for serialization
+  val setDoc = bucket.set[JsObject]("john-doe", document)
+
+  setDoc.onSuccess {
+    case status => println(s"Operation status : ${status.getMessage}")
   }
 
-  for (i <- 1 to 100) {
-    Await.result(bucket.delete(s"person-$i"), timeout)
+  // persist the Person instance with the key 'jane-doe', using implicit 'personFmt' for serialization
+  val setInstance = bucket.set[Person]("jane-doe", Person("Jane", "Doe", 42))
+
+  setInstance.onSuccess {
+    case status => println(s"Operation status : ${status.getMessage}")
   }
 
-  driver.shutdown()
+  // get the Person instance with the key 'john-doe', using implicit 'personFmt' for deserialization
+  val getDoc = bucket.get[Person]("john-doe").map { opt =>
+    println(opt.map(person => s"Found John : ${person}").getOrElse("Cannot find object with key 'john-doe'"))
+  }
+
+  // delete docs with keys "john-doe" and "jane-doe"
+  val deleteDoc = Future.sequence(Seq(bucket.delete("john-doe"), bucket.delete("jane-doe")))
+
+  deleteDoc.onSuccess {
+    case statuses => println(s"""Operations status : ${statuses.map(_.getMessage).mkString(", ")}""")
+  }
+
+  val futureMessage = for {
+    _ <- setDoc
+    _ <- setInstance
+    message <- getDoc
+    _ <- deleteDoc
+  } yield message
+
+  futureMessage map { message => println(message) } map { _ => driver.shutdown() }  
 
 }
