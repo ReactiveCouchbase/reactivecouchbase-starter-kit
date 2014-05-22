bf68dd221aeb132ad68bc8e6124e1c0c594e040c
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2014-05-22 13:17:56 +0200

    Update to 0.3-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object ApplicationBuild extends Build {
 
   val appName         = "starterkit"
-  val appVersion      = "0.2"
+  val appVersion      = "0.3-SNAPSHOT"
   val appScalaVersion = "2.10.2"
   val appScalaBinaryVersion = "2.10"
   val appScalaCrossVersions = Seq("2.10.2")
@@ -30,8 +30,8 @@ object ApplicationBuild extends Build {
     .settings(
       resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
       resolvers += "ReactiveCouchbase" at "https://raw.github.com/ReactiveCouchbase/repository/master/snapshots",
-      libraryDependencies += "org.reactivecouchbase" %% "reactivecouchbase-core" % "0.2",
-      libraryDependencies += "org.reactivecouchbase" %% "reactivecouchbase-es" % "0.2",
+      libraryDependencies += "org.reactivecouchbase" %% "reactivecouchbase-core" % "0.3-SNAPSHOT",
+      libraryDependencies += "org.reactivecouchbase" %% "reactivecouchbase-es" % "0.3-SNAPSHOT",
       libraryDependencies += "org.specs2" %% "specs2" % "2.2.1" % "test",
       organization := "foo.bar",
       version := appVersion,
